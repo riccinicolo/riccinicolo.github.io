9faeae2fe8f77e52c1af32886b5e56b7ef232058
riccinicolo <82954917+riccinicolo@users.noreply.github.com>
2022-10-13 14:16:00 +0200

    padding again

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "daef7e23e7ff9d999196b59b29744a73",
-"index.html": "53eaaa8c9ec4226d70cfd86ffb3c05cc",
-"/": "53eaaa8c9ec4226d70cfd86ffb3c05cc",
-"main.dart.js": "d84cfc0c7f967303ba79bd247ee895b5",
+"index.html": "6ef5b5a4afc6074f26c30f5c1a61699f",
+"/": "6ef5b5a4afc6074f26c30f5c1a61699f",
+"main.dart.js": "7645e0570075035c7d465bd134c07939",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
